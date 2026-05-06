4cf0641b926ca601e47124e4bc1f57a5331ab4bf
Angel David Villa Luján <escarlata.2003@gmail.com>
2026-05-06 10:10:00 -0500

    fix: se agregaron mas productos y diferentes categorias

--- backend/src/data/store.ts
+++ backend/src/data/store.ts
@@ -5,7 +5,7 @@ import { Producto } from '../models/producto.model';
 // IA-DECISION: Uso de mock data para cumplir el requerimiento sin BD real.
 
 export const productos: Producto[] = [
-  {
+   {
     id: '1',
     nombre: 'Laptop',
     categoria: 'tecnologia',
@@ -21,12 +21,52 @@ export const productos: Producto[] = [
     stockActual: 2,
     stockMinimo: 5
   },
-  {
-    id: '3',
+   {
+    id: '4',
     nombre: 'Silla',
     categoria: 'hogar',
     precio: 200,
     stockActual: 10,
     stockMinimo: 4
+  },
+  {
+    id: '5',
+    nombre: 'Mesa',
+    categoria: 'hogar',
+    precio: 500,
+    stockActual: 3,
+    stockMinimo: 2
+  },
+  {
+    id: '6',
+    nombre: 'Lámpara',
+    categoria: 'hogar',
+    precio: 150,
+    stockActual: 1,
+    stockMinimo: 3
+    },
+  {
+    id: '7',
+    nombre: 'Arroz',
+    categoria: 'alimentos',
+    precio: 30,
+    stockActual: 20,
+    stockMinimo: 10
+  },
+  {
+    id: '8',
+    nombre: 'Leche',
+    categoria: 'alimentos',
+    precio: 15,
+    stockActual: 6,
+    stockMinimo: 8
+  },
+  {
+    id: '9',
+    nombre: 'Pan',
+    categoria: 'alimentos',
+    precio: 10,
+    stockActual: 12,
+    stockMinimo: 5
   }
 ];
