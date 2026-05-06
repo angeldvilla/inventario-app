6e10573afcd15366da460f233524dfb75be6ce6e
Angel David Villa Luján <escarlata.2003@gmail.com>
2026-05-06 11:28:38 -0500

    fix: permitir peticiones externas usando CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,14 @@ import alertaRoutes from './routes/alerta.route';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+}); // CORS 
+
 
 /* Ruta de prueba */
 app.get("/health", (_req, res) => {
